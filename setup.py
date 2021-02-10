6f06fadb05b6ab55403efa013b3d2b8b7f7c004e
atarashansky <tarashan@stanford.edu>
2021-02-10 10:05:59 -0800

    adding files

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+__version__ = "0.0.1"
+
+
+setup(
+    name="SCTransformPy",
+    version=__version__,
+    author="Alexander Tarashansky",
+    author_email="atarashansky@illumina.com",
+    url="",
+    description="Python port of SCTransform from the Seurat package.",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "statsmodels",
+        "KDEpy",
+        "pandas",
+        "anndata",        
+    ],
+    py_modules=['SCTransform.py']
+)
